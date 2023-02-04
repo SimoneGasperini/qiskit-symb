863932356d10016a4e9a86788d94cb9d15b6cb49
SimoneGasperini <simone.gasperini4@unibo.it>
2023-02-04 15:20:34 +0100

    Set up GitHub Actions workflows for testing and linting

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
+"""Python package installer"""
+
 import setuptools
 
 
-with open('requirements.txt') as file:
+with open('requirements.txt', encoding='utf-8') as file:
     install_requires = file.read()
 
 setuptools.setup(
     name='qiskit-symbolic',
+    version='0.0.1',
+    url='https://github.com/SimoneGasperini/qiskit-symbolic',
     author='SimoneGasperini',
     author_email='simone.gasperini4@unibo.it',
+    license="Apache 2.0",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
-    python_requires='>=3.9'
+    python_requires='>=3.8'
 )
