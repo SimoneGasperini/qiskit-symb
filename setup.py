c15e0f26dc168675c7c6adf4e8064eaacec70124
SimoneGasperini <simone.gasperini4@unibo.it>
2023-02-05 01:02:42 +0100

    Fix configuration for Python project

--- setup.py
+++ setup.py
@@ -5,14 +5,6 @@ with open('requirements.txt', encoding='utf-8') as file:
     install_requires = file.read()
 
 setuptools.setup(
-    name='qiskit-symbolic',
-    version='0.0.1',
-    url='https://github.com/SimoneGasperini/qiskit-symbolic',
-    author='SimoneGasperini',
-    author_email='simone.gasperini4@unibo.it',
-    license='Apache 2.0',
-    package_dir={'': 'src'},
     packages=setuptools.find_packages(),
-    install_requires=install_requires,
-    python_requires='>=3.8',
+    install_requires=install_requires
 )
