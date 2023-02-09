415e5bbd54aef2354176a994c24c0b9da78e10ba
SimoneGasperini <simone.gasperini4@unibo.it>
2023-02-09 14:08:36 +0100

    Add unit tests for standard gates

---
+++ tests/test_standard_gates.py
@@ -0,0 +1,78 @@
+"""Test standard gates module"""
+
+import numpy
+from qiskit.circuit.library import IGate, XGate, YGate, ZGate, HGate, SGate, SdgGate, TGate, TdgGate
+from qiskit_symbolic.library import (
+    IGate as symb_IGate,
+    XGate as symb_XGate,
+    YGate as symb_YGate,
+    ZGate as symb_ZGate,
+    HGate as symb_HGate,
+    SGate as symb_SGate,
+    SdgGate as symb_SdgGate,
+    TGate as symb_TGate,
+    TdgGate as symb_TdgGate
+)
+
+
+def test_id():
+    """todo"""
+    arr1 = IGate().to_matrix()
+    arr2 = symb_IGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
+
+
+def test_x():
+    """todo"""
+    arr1 = XGate().to_matrix()
+    arr2 = symb_XGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
+
+
+def test_y():
+    """todo"""
+    arr1 = YGate().to_matrix()
+    arr2 = symb_YGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
+
+
+def test_z():
+    """todo"""
+    arr1 = ZGate().to_matrix()
+    arr2 = symb_ZGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
+
+
+def test_h():
+    """todo"""
+    arr1 = HGate().to_matrix()
+    arr2 = symb_HGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
+
+
+def test_s():
+    """todo"""
+    arr1 = SGate().to_matrix()
+    arr2 = symb_SGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
+
+
+def test_sdg():
+    """todo"""
+    arr1 = SdgGate().to_matrix()
+    arr2 = symb_SdgGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
+
+
+def test_t():
+    """todo"""
+    arr1 = TGate().to_matrix()
+    arr2 = symb_TGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
+
+
+def test_tdg():
+    """todo"""
+    arr1 = TdgGate().to_matrix()
+    arr2 = symb_TdgGate().to_matrix()
+    assert numpy.allclose(arr1, arr2)
