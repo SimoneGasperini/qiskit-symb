7dab24e84cffaf2971f98766035bb8a8e130c5e2
SimoneGasperini <simone.gasperini4@unibo.it>
2023-03-11 10:37:25 +0100

    Fix random_parametric_circuit generation function

--- src/qiskit_symbolic/random.py
+++ src/qiskit_symbolic/random.py
@@ -3,7 +3,7 @@
 import random
 import math
 from qiskit import QuantumRegister, QuantumCircuit
-from qiskit.circuit import ParameterVector, CircuitInstruction, Qubit
+from qiskit.circuit import ParameterVector, Qubit
 from qiskit.circuit.library import standard_gates
 from .utils import get_symbolic_gates_names
 
@@ -11,10 +11,13 @@ from .utils import get_symbolic_gates_names
 def random_parametric_circuit(num_qubits, depth, seed=None):
     """todo"""
     # pylint: disable=too-many-locals
-    random.seed(seed)
+    register = QuantumRegister(num_qubits)
+    circuit = QuantumCircuit(register)
+    params = ParameterVector(name='x', length=0)
+    if num_qubits == 0 or depth == 0:
+        return circuit, params
     qiskit_gates = {
         # gate_name: (gate_class, num_qubits, num_params)
-        'id': (standard_gates.IGate, 1, 0),
         'sx': (standard_gates.SXGate, 1, 0),
         'x': (standard_gates.XGate, 1, 0),
         'rz': (standard_gates.RZGate, 1, 1),
@@ -34,37 +37,38 @@ def random_parametric_circuit(num_qubits, depth, seed=None):
         'u3': (standard_gates.U3Gate, 1, 3),
         'y': (standard_gates.YGate, 1, 0),
         'z': (standard_gates.ZGate, 1, 0),
-        'cx': (standard_gates.CXGate, 2, 0),
-        'dcx': (standard_gates.DCXGate, 2, 0),
-        'ch': (standard_gates.CHGate, 2, 0),
-        'cp': (standard_gates.CPhaseGate, 2, 1),
-        'crx': (standard_gates.CRXGate, 2, 1),
-        'cry': (standard_gates.CRYGate, 2, 1),
-        'crz': (standard_gates.CRZGate, 2, 1),
-        'csx': (standard_gates.CSXGate, 2, 0),
-        # 'cu': (standard_gates.CUGate, 2, 4), https://github.com/Qiskit/qiskit-terra/issues/9763
-        'cu1': (standard_gates.CU1Gate, 2, 1),
-        'cu3': (standard_gates.CU3Gate, 2, 3),
-        'cy': (standard_gates.CYGate, 2, 0),
-        'cz': (standard_gates.CZGate, 2, 0),
-        'rxx': (standard_gates.RXXGate, 2, 1),
-        'ryy': (standard_gates.RYYGate, 2, 1),
-        'rzz': (standard_gates.RZZGate, 2, 1),
-        'rzx': (standard_gates.RZXGate, 2, 1),
-        'xx_minus_yy': (standard_gates.XXMinusYYGate, 2, 2),
-        'xx_plus_yy': (standard_gates.XXPlusYYGate, 2, 2),
-        'ecr': (standard_gates.ECRGate, 2, 0),
-        'cs': (standard_gates.CSGate, 2, 0),
-        'csdg': (standard_gates.CSdgGate, 2, 0),
-        'swap': (standard_gates.SwapGate, 2, 0),
-        'iswap': (standard_gates.iSwapGate, 2, 0)
     }
+    if num_qubits > 1:
+        qiskit_gates.update({
+            'cx': (standard_gates.CXGate, 2, 0),
+            'dcx': (standard_gates.DCXGate, 2, 0),
+            'ch': (standard_gates.CHGate, 2, 0),
+            'cp': (standard_gates.CPhaseGate, 2, 1),
+            'crx': (standard_gates.CRXGate, 2, 1),
+            'cry': (standard_gates.CRYGate, 2, 1),
+            'crz': (standard_gates.CRZGate, 2, 1),
+            'csx': (standard_gates.CSXGate, 2, 0),
+            # https://github.com/Qiskit/qiskit-terra/issues/9763
+            # 'cu': (standard_gates.CUGate, 2, 4),
+            'cu1': (standard_gates.CU1Gate, 2, 1),
+            'cu3': (standard_gates.CU3Gate, 2, 3),
+            'cy': (standard_gates.CYGate, 2, 0),
+            'cz': (standard_gates.CZGate, 2, 0),
+            'rxx': (standard_gates.RXXGate, 2, 1),
+            'ryy': (standard_gates.RYYGate, 2, 1),
+            'rzz': (standard_gates.RZZGate, 2, 1),
+            'rzx': (standard_gates.RZXGate, 2, 1),
+            'xx_minus_yy': (standard_gates.XXMinusYYGate, 2, 2),
+            'xx_plus_yy': (standard_gates.XXPlusYYGate, 2, 2),
+            'ecr': (standard_gates.ECRGate, 2, 0),
+            'cs': (standard_gates.CSGate, 2, 0),
+            'csdg': (standard_gates.CSdgGate, 2, 0),
+            'swap': (standard_gates.SwapGate, 2, 0),
+            'iswap': (standard_gates.iSwapGate, 2, 0)
+        })
+    random.seed(seed)
     qiskit_gates_names = list(qiskit_gates.keys())
     symbolic_gates_names = get_symbolic_gates_names()
-    register = QuantumRegister(num_qubits)
-    params = ParameterVector(name='x', length=0)
-    instructions = []
-    circuit = QuantumCircuit.from_instructions(instructions)
     while circuit.depth() != depth:
         gate_name = random.choice(qiskit_gates_names)
         gate_init, n_qubits, n_params = qiskit_gates[gate_name]
@@ -82,6 +86,5 @@ def random_parametric_circuit(num_qubits, depth, seed=None):
         qubits = [Qubit(register=register,
                         index=indices.pop(random.randrange(len(indices))))
                   for _ in range(n_qubits)]
-        instructions.append(CircuitInstruction(gate, qubits))
-        circuit = QuantumCircuit.from_instructions(instructions)
+        circuit.append(instruction=gate, qargs=qubits)
     return circuit, params
