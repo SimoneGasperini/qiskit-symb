7e77d1c44e88da9487e0cae27f845ef01ee16ed0
SimoneGasperini <simone.gasperini4@unibo.it>
2023-03-17 09:33:42 +0100

    Improve quantum_info testing by using pytest.fixture decorator

--- src/qiskit_symbolic/utils.py
+++ src/qiskit_symbolic/utils.py
@@ -1,5 +1,6 @@
 """Utilities module"""
 
+import random
 from sympy import Symbol, sympify
 from qiskit import QuantumCircuit, transpile
 from .circuit.library import NAME_TO_INIT  # pylint: disable=cyclic-import
@@ -53,3 +54,15 @@ def symbols2real(sympy_expr):
     args_dict = {symbol: Symbol(symbol.name, real=True)
                  for symbol in sympy_expr.free_symbols}
     return sympy_expr.subs(args_dict)
+
+
+def get_random_params(params_dict, size, seed=None):
+    """todo"""
+    random.seed(seed)
+    parnames = list(params_dict.keys())
+    params = list(zip(*[[random.randint(*params_dict[parname]) for _ in range(size)]
+                        for parname in parnames]))
+    ids = [','.join([f'{parname}={parval}'
+                     for parname, parval in zip(parnames, parvals)])
+           for parvals in params]
+    return params, ids
--- tests/test_operator.py
+++ tests/test_operator.py
@@ -1,25 +1,34 @@
 """Test operator module"""
 
 import numpy
-from hypothesis import given, settings, strategies
+import pytest
 from qiskit.quantum_info import Operator
 from qiskit_symbolic.circuit.random import random_parametric_circuit
 from qiskit_symbolic.quantum_info import Operator as symb_Operator
+from qiskit_symbolic.utils import get_random_params
 
 
-@given(num_qubits=strategies.integers(min_value=1, max_value=3),
-       seed=strategies.integers(min_value=0, max_value=2**32-1))
-@settings(max_examples=10, deadline=None)
-def test_from_circuit(num_qubits, seed):
+# pylint: disable=duplicate-code
+testing_params = {'num_qubits': (1, 3), 'seed': (0, 999)}
+pars, ids = get_random_params(testing_params, size=10)
+
+
+@pytest.fixture(scope='module', params=pars, ids=ids)
+def _test_data(request):
     """todo"""
-    # pylint: disable=duplicate-code
-    numpy.random.seed(seed)
+    num_qubits, seed = request.param
     circuit, params = random_parametric_circuit(
         num_qubits=num_qubits, depth=4, seed=seed)
+    symb_op = symb_Operator(circuit)
+    return circuit, params, symb_op
+
+
+def test_from_circuit(_test_data):
+    """todo"""
+    circuit, params, symb_op = _test_data
     values = numpy.random.rand(len(params)) * 2*numpy.pi
     params_dict = dict(zip(params, values))
     qiskit_circ = circuit.assign_parameters(params_dict)
     arr1 = Operator(qiskit_circ).data
-    symb_op = symb_Operator(circuit)
     arr2 = symb_op.subs(params_dict).to_numpy()
     assert numpy.allclose(arr1, arr2)
