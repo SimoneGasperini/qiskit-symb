9ad8865dfeaaad2ce16c5b17645bbc9340c16d95
SimoneGasperini <simone.gasperini4@unibo.it>
2026-03-11 13:09:52 +0100

    Implement block-wise circuit splitting to reduce compilation time

--- src/qiskit_symb/simulator.py
+++ src/qiskit_symb/simulator.py
@@ -1,100 +1,194 @@
-"""Layer-wise symbolic simulator module."""
+"""Symbolic simulator module."""
 
-from dataclasses import dataclass
-from functools import reduce
 import numpy
-from qiskit.circuit import QuantumCircuit, ParameterVector
-from qiskit.converters import circuit_to_dag, dag_to_circuit
-from qiskit.quantum_info import Statevector as QiskitStatevector
-from .quantum_info import Operator
-
-
-def _split_by_barriers(circ):
-    """todo"""
-    nq, nc = circ.num_qubits, circ.num_clbits
-    circ_list = [QuantumCircuit(nq, nc)]
-    for ci in circ:
-        sub_circ = circ_list[-1]
-        op = ci.operation
-        qubits = [sub_circ.qubits[circ.find_bit(q).index] for q in ci.qubits]
-        clbits = [sub_circ.clbits[circ.find_bit(c).index] for c in ci.clbits]
-        if op.name == "barrier":
-            circ_list.append(QuantumCircuit(nq, nc))
-        else:
-            sub_circ.append(op, qargs=qubits, cargs=clbits)
-    # global phase?
-    return circ_list
+import sympy
+from qiskit.circuit import ParameterVector
+from .quantum_info.mpo import _layers_to_symbolic_gates, _preprocess_circuit
 
 
 def _flat_params_dict(params_dict):
-    """todo"""
+    """Expand ParameterVector keys into scalar parameter entries."""
     flat_params = {}
-    for param, values in params_dict.items():
+    for param, param_values in params_dict.items():
         if isinstance(param, ParameterVector):
-            for p, values in zip(param, zip(*values)):
-                flat_params[p] = values
+            for scalar_param, scalar_values in zip(param, zip(*param_values)):
+                flat_params[scalar_param] = scalar_values
         else:
-            flat_params[param] = values
+            flat_params[param] = param_values
     return flat_params
 
 
-@dataclass
 class CompiledCircuit:
-    """todo"""
+    """Executable compiled gates for repeated statevector evaluations."""
 
-    num_qubits: int
-    layer_funcs: list
-    layer_args: list
+    def __init__(self, num_qubits, gates, global_phase):
+        """Store compiled data used by Simulator.run()."""
+        self.num_qubits = num_qubits
+        self.gates = gates
+        self.global_phase = global_phase
+
+
+def _gate_matrix(gate):
+    """Return symbolic matrix representation of one gate."""
+    dim = 2**gate.nqubits
+    return numpy.array(gate._get_tensor_array().tolist(), dtype=object).reshape(
+        dim, dim
+    )
+
+
+def _build_blocks(gates, max_block_qubits, max_block_gates):
+    """Fuse consecutive gates on the same qubits into small compilation blocks."""
+    blocks = []
+    current_qubits = None
+    current_gates = []
+    for gate in gates:
+        gate_qubits = tuple(gate.qubits)
+        must_start_new_block = (
+            not current_gates
+            or gate_qubits != current_qubits
+            or len(current_gates) >= max_block_gates
+            or len(gate_qubits) > max_block_qubits
+        )
+        if must_start_new_block:
+            if current_gates:
+                blocks.append((current_qubits, current_gates))
+            current_qubits = gate_qubits
+            current_gates = [gate]
+        else:
+            current_gates.append(gate)
+    if current_gates:
+        blocks.append((current_qubits, current_gates))
+    return blocks
+
+
+def _compile_block(block_qubits, block_gates, ordered_params):
+    """Return compiled block data as tuple: (matrix_or_func, args_or_none, qubits, dim)."""
+    dim = 2 ** len(block_qubits)
+    block_matrix = numpy.eye(dim, dtype=object)
+    for gate in block_gates:
+        block_matrix = _gate_matrix(gate) @ block_matrix
+    sympy_matrix = sympy.Array(block_matrix.tolist())
+    free_symbols = sympy_matrix.free_symbols
+    if not free_symbols:
+        const_matrix = numpy.array(block_matrix, dtype=complex)
+        return const_matrix, None, block_qubits, dim
+
+    name2symb = {symb.name: symb for symb in free_symbols}
+    args = tuple(par for par in ordered_params if par.name in name2symb)
+    sympy_args = [name2symb[par.name] for par in args]
+    func = sympy.lambdify(
+        args=sympy_args, expr=sympy_matrix, modules="numpy", dummify=True, cse=True
+    )
+    return func, args, block_qubits, dim
+
+
+def _compile_global_phase(phase_expr, ordered_params):
+    """Return (value_or_func, args_or_none) for a circuit global phase."""
+    phase = sympy.exp(sympy.I * phase_expr)
+    free_symbols = phase.free_symbols
+    if not free_symbols:
+        return complex(phase), None
+
+    name2symb = {symb.name: symb for symb in free_symbols}
+    args = tuple(par for par in ordered_params if par.name in name2symb)
+    sympy_args = [name2symb[par.name] for par in args]
+    func = sympy.lambdify(
+        args=sympy_args, expr=phase, modules="numpy", dummify=True, cse=True
+    )
+    return func, args
+
+
+def _apply_gate_matrix_to_state(state, matrix, qubits, num_qubits, gate_dim):
+    """Apply a k-qubit matrix to a full statevector."""
+    tensor = state.reshape((2,) * num_qubits)
+    gate_axes = [num_qubits - 1 - qubit for qubit in qubits]
+    keep_axes = [axis for axis in range(num_qubits) if axis not in gate_axes]
+    perm = keep_axes + gate_axes
+    inv_perm = numpy.argsort(perm)
+    permuted = numpy.transpose(tensor, perm)
+    flat = permuted.reshape(-1, gate_dim)
+    updated = flat @ matrix.T
+    return numpy.transpose(updated.reshape(permuted.shape), inv_perm).reshape(
+        2**num_qubits
+    )
 
 
 class Simulator:
-    """Layer-wise symbolic simulator for repeated circuit evaluations."""
+    """Symbolic simulator for repeated circuit evaluations."""
 
-    def __init__(self, backend="numpy", splitting=None):
-        """todo"""
-        self._backend = backend
-        self._splitting = splitting
-        if splitting is not None:
-            if splitting not in ["layers", "barriers"]:
-                raise ValueError(
-                    f"`splitting={splitting}` is not valid. Use 'layers' or 'barriers'."
-                )
+    def __init__(self, max_block_qubits=4, max_block_gates=6):
+        """Create a simulator with lightweight same-qubit gate fusion."""
+        self._max_block_qubits = max_block_qubits
+        self._max_block_gates = max_block_gates
 
     @staticmethod
     def _run_once(compiled_circ, arg_values):
-        """todo"""
-        nq = compiled_circ.num_qubits
-        zeros_state = QiskitStatevector.from_label("0" * nq).data
-        unitaries = (
-            func(*(arg_values[arg] for arg in args))
-            for func, args in zip(compiled_circ.layer_funcs, compiled_circ.layer_args)
-        )
-        return reduce(lambda psi, u: numpy.matmul(u, psi), unitaries, zeros_state)
+        """Evaluate one parameter assignment and return the output statevector."""
+        num_qubits = compiled_circ.num_qubits
+        state = numpy.zeros(2**num_qubits, dtype=complex)
+        state[0] = 1.0
+        for matrix_or_func, args_or_none, qubits, dim in compiled_circ.gates:
+            if args_or_none is None:
+                matrix = matrix_or_func
+            else:
+                matrix = numpy.array(
+                    matrix_or_func(*(arg_values[arg] for arg in args_or_none)),
+                    dtype=complex,
+                ).reshape(dim, dim)
+            state = _apply_gate_matrix_to_state(
+                state=state,
+                matrix=matrix,
+                qubits=qubits,
+                num_qubits=num_qubits,
+                gate_dim=dim,
+            )
+        phase_or_func, phase_args = compiled_circ.global_phase
+        if phase_args is None:
+            state = phase_or_func * state
+        else:
+            state = phase_or_func(*(arg_values[arg] for arg in phase_args)) * state
+        return state
 
     def compile(self, circ):
-        """todo"""
-        if self._splitting == "layers":
-            layers = circuit_to_dag(circ).layers()
-            operators = [Operator(dag_to_circuit(lay["graph"])) for lay in layers]
-        elif self._splitting == "barriers":
-            circuits = _split_by_barriers(circ)
-            operators = [Operator(qc) for qc in circuits]
-        else:
-            operators = [Operator(circ)]
+        """Compile a circuit into reusable symbolic gate evaluators."""
+        transpiled_circ = _preprocess_circuit(circ)
+        symbolic_layers = _layers_to_symbolic_gates(transpiled_circ)
+        gates = []
+        for layer in symbolic_layers:
+            gates.extend(layer)
+        blocks = _build_blocks(
+            gates=gates,
+            max_block_qubits=self._max_block_qubits,
+            max_block_gates=self._max_block_gates,
+        )
+        params = tuple(circ.parameters)
+        compiled_gates = []
+        for block_qubits, block_gates in blocks:
+            compiled_gates.append(
+                _compile_block(
+                    block_qubits=block_qubits,
+                    block_gates=block_gates,
+                    ordered_params=params,
+                )
+            )
         return CompiledCircuit(
             num_qubits=circ.num_qubits,
-            layer_funcs=[op.to_lambda() for op in operators],
-            layer_args=[op._params for op in operators],
+            gates=compiled_gates,
+            global_phase=_compile_global_phase(
+                phase_expr=transpiled_circ.global_phase, ordered_params=params
+            ),
         )
 
     def run(self, compiled_circ, params_dict):
-        """todo"""
+        """Evaluate a compiled circuit for multiple parameter assignments."""
         flat_params = _flat_params_dict(params_dict=params_dict)
-        num_reps = len(next(iter(flat_params.values())))
-        return [
-            self._run_once(
-                compiled_circ=compiled_circ,
-                arg_values={arg: values[i] for arg, values in flat_params.items()},
+        num_reps = len(next(iter(flat_params.values()))) if flat_params else 1
+        results = []
+        for i in range(num_reps):
+            arg_values = {}
+            for arg, values in flat_params.items():
+                arg_values[arg] = values[i]
+            results.append(
+                self._run_once(compiled_circ=compiled_circ, arg_values=arg_values)
             )
-            for i in range(num_reps)
-        ]
+        return results
--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,25 +1,20 @@
 """Test symbolic simulator module."""
 
 import numpy
-import pytest
 from hypothesis import given, settings, strategies
 from qiskit.quantum_info import Statevector
 from qiskit_symb import Simulator
 from qiskit_symb.circuit.random import random_parametric_circuit
 
 
-@pytest.mark.parametrize("splitting", ["layers", "barriers"])
-@given(num_qubits=strategies.integers(min_value=1, max_value=5))
+@given(num_qubits=strategies.integers(min_value=1, max_value=6))
 @settings(deadline=None)
-def test_execution(num_qubits, splitting):
+def test_execution(num_qubits):
     """todo"""
-    pqc1 = random_parametric_circuit(num_qubits, par_prefix="x", depth=2)
-    pqc1.barrier()
-    pqc2 = random_parametric_circuit(num_qubits, par_prefix="y", depth=3)
-    pqc = pqc1.compose(pqc2)
+    pqc = random_parametric_circuit(num_qubits, depth=4)
     params = tuple(pqc.parameters)
     data = numpy.random.rand(5, len(params)) * 2 * numpy.pi
-    simulator = Simulator(splitting=splitting)
+    simulator = Simulator()
     compiled_qc = simulator.compile(pqc)
     params_dict = dict(zip(params, data.transpose()))
     results = simulator.run(compiled_qc, params_dict=params_dict)
